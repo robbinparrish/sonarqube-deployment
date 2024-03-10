0833154b583ad7b36f7849b6f2ef1a024f5fbc58
robbinparrish <1752658+robbinparrish@users.noreply.github.com>
2024-03-10 15:45:49 +0530

    Updated timestamp and added compression in backup. (#4)

--- configs/automatic-backup/sonarqube-deployment-backup.sh
+++ configs/automatic-backup/sonarqube-deployment-backup.sh
@@ -6,7 +6,7 @@ SONARQUBE_DEPLOYMENT_DIR="${HOME}/sonarqube-deployment"
 SONARQUBE_DEPLOYMENT_BACKUP_PATH="${HOME}/sonarqube-deployment-backup"
 
 # Backup directory.
-BACKUP_TIME=$(date "+%a-%d-%m-%Y-%k-%M")
+BACKUP_TIME=$(date "+%d%m%Y%H%M%S")
 SONARQUBE_DEPLOYMENT_BACKUP_DIR="${SONARQUBE_DEPLOYMENT_BACKUP_PATH}/${BACKUP_TIME}"
 
 cd "${SONARQUBE_DEPLOYMENT_DIR}" || exit 1
@@ -20,4 +20,10 @@ mkdir -p ./pgsql-db/backups
 chown -R 1000:1000 ./pgsql-db/backups
 docker-compose exec -it pgsql-db /usr/bin/pg_dumpall --clean -U postgres > ./pgsql-db/backups/pgsql-db-backup.sql
 mv -fv ./pgsql-db/backups/pgsql-db-backup.sql "${SONARQUBE_DEPLOYMENT_BACKUP_DIR}"/
-
+sync
+ 
+# Compress the backup.
+cd "${SONARQUBE_DEPLOYMENT_BACKUP_PATH}" || exit 1
+tar -cvzf "${BACKUP_TIME}".tar.gz "${BACKUP_TIME}"
+rm -rf "${BACKUP_TIME}"
+sync
